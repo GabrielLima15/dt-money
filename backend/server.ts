710640cf1c6c37812fa4d0be023d8e8ed52cf67b
GabrielLima15 <gabrielcarvalho850@outlook.com>
2023-08-24 00:09:07 -0300

    criando rota para post e para get

--- backend/server.ts
+++ backend/server.ts
@@ -3,13 +3,27 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 const server = jsonServer.create();
-const router = jsonServer.router('server.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
 
-const port = process.env.PORT || 3333; // Use the port provided by Vercel or default to 3333
+const jsonFilePath = path.join(__dirname, 'server.json');
+const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+
+server.get('/transactions', (req, res) => {
+  res.json(jsonData.transactions);
+});
+
+server.post('/transactions', (req, res) => {
+  const newTransaction = req.body;
+  jsonData.transactions.push(newTransaction);
+
+  fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
+
+  res.status(201).json(newTransaction);
+});
+
+const port = process.env.PORT || 3333;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
